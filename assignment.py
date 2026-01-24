e7fa793f17333c9454bb569ef405cc8024db0bef
drashti-thakar <drashtithakar145@gmail.com>
2026-01-24 11:35:34 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,73 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print("Drashti Thakar")
+Drashti Thakar
+\
+a=(input("enter the value: "))
+enter the value: 14
+b=(input("enter the value: "))
+enter the value: 25
+sum=int(a)+int(b)
+print(sum)
+39
+
+num=int(input("enter the num: "))
+enter the num: 14
+if num%2==0:
+...     print("this number is even")
+... else:
+...     print("this number is odd")
+... 
+...     
+this number is even
+>>> 
+>>> year=int(input("enter a year: "))
+enter a year: 2003
+>>> if(year %4==0 and year %100!=0):
+...     print("leap year")
+... else:
+...     print("not a leap year")
+... 
+...     
+not a leap year
+>>> 
+>>> import math
+>>> print(round(math.pi, 2))
+3.14
+>>> 
+>>> pi=3.14
+>>> print(pi)
+3.14
+>>> 
+>>> num=5
+print(num*num)
+25
+
+radius=float(input("enter radius: "))
+enter radius: 7
+area=3.14*radius*radius
+print("area of circle= ",area)
+area of circle=  153.86
+
+a=10
+print(type(a))
+<class 'int'>
+
+import math
+print(math.sqrt(16))
+4.0
+
+print(pow(2,3))
+8
+
+num=int(input("enter a number: "))
+enter a number: 0
+if num>0:
+    print("number is positive")
+elif num<0:
+    print("number is negative")
+else:
+    print("number is zero")
+
+    
+number is zero
