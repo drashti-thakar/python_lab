15bcfd715552d26bcf4207200f721b3d512ebe24
drashti-thakar <drashtithakar145@gmail.com>
2026-01-26 13:07:30 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,77 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#Arithmetic operator
+a=14
+b=5
+print(a+b)
+19
+print(a-b)
+9
+print(a*b)
+70
+print(a/b)
+2.8
+
+#Assignment operator
+x=14
+print(x)
+14
+
+#Unary operator
+x=5
+print(-x)
+-5
+
+#relational operator
+p=14
+q=25
+print(p<q)
+True
+print(p>q)
+False
+print(p==q)
+False
+
+#logical operator
+p=True
+q=False
+print(p and q)
+False
+print(p or q)
+True
+print(not p)
+False
+print(not q)
+True
+
+#boolean operator
+is_pass=True
+print(is_pass)
+True
+
+#bitwise operator
+a=14
+b=5
+print(a&b)
+4
+print(a|b)
+15
+>>> 
+>>> #membership operator
+>>> numbers=[1,2,3,4,5]
+>>> print(5 is numbers)
+False
+>>> print(5 in numbers)
+True
+>>> print(14 in numbers)
+False
+>>> print(14 not in numbers)
+True
+>>> 
+>>> #identity operator
+>>> x=25
+>>> y=25
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
