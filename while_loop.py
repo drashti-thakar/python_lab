a29a7b289fefdca5893c2cd131917f456406771d
drashti-thakar <drashtithakar145@gmail.com>
2026-02-24 08:17:46 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,77 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+
+    
+1
+2
+3
+4
+5
+#print numbers from 1 to 10
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+
+    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+#sum of first in n natural numbers
+n=int (input("enter n:"))
+enter n:14
+i=1
+s=0
+while i<=n
+SyntaxError: expected ':'
+while i<=n:
+    s=s+i
+    i=i+1
+    print("sum= ",s)
+
+    
+sum=  1
+sum=  3
+sum=  6
+sum=  10
+sum=  15
+sum=  21
+sum=  28
+sum=  36
+sum=  45
+sum=  55
+sum=  66
+sum=  78
+sum=  91
+sum=  105
+>>> #table of a number
+>>> num=int(input("enter number: "))
+enter number: 25
+>>> i=1
+>>> while i<=10:
+...     print(num,"x",i,"=",num*i)
+...     i=i+1
+... 
+...     
+25 x 1 = 25
+25 x 2 = 50
+25 x 3 = 75
+25 x 4 = 100
+25 x 5 = 125
+25 x 6 = 150
+25 x 7 = 175
+25 x 8 = 200
+25 x 9 = 225
+25 x 10 = 250
